71ae7ba270d15cfa3b6dda91c16ba1416aa4fb9d
Saravanan Balasubramanian <sarabala1979@gmail.com>
2025-04-23 18:20:28 -0700

    feat: initial commit with all project files

---
+++ internal/scheduler/types.go
@@ -0,0 +1,20 @@
+package scheduler
+
+import (
+	sarabalaiov1alpha1 "github.com/sarabala1979/SmartHPA/api/v1alpha1"
+)
+
+// SchedulerInterface defines the interface for scheduler implementations
+type SchedulerInterface interface {
+	// Start starts the scheduler
+	Start()
+
+	// Stop stops the scheduler
+	Stop()
+
+	// AddOrUpdateSchedules processes triggers from a SmartHPA object and updates the schedule map
+	AddOrUpdateSchedules(shpa *sarabalaiov1alpha1.SmartHorizontalPodAutoscaler) error
+
+	// GetActiveSchedules returns all currently active schedules
+	GetActiveSchedules() []*TriggerSchedule
+}
